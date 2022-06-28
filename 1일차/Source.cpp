daf704f6cc79e8cb6ed72cc42c81466cb5f70517
Chanyoo Kim <cksdb012@gmail.com>
2022-06-28 22:24:29 +0900

    1일차

--- 1일차/Source.cpp
+++ 1일차/Source.cpp
@@ -1,22 +1,22 @@
-//#include <iostream>
-//
-//int func1(int& a) {
-//    a++;
-//    return a;
-//}
-//
-//int& func2(int& a) {
-//    a++;
-//    return a;
-//}
-//
-//int main() {
-//    int x = 10;
-//    int y = 20;
-//    int& j = x;
-//
-//    std::cout << func1(x) << std::endl;
-//    std::cout << func2(y) << std::endl;
-//    std::cout << j << std::endl;
-//    std::cout << y << std::endl;
-//}
+#include <iostream>
+
+int func1(int& a) {
+    a++;
+    return a;
+}
+
+int& func2(int& a) {
+    a++;
+    return a;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+    int& j = x;
+
+    std::cout << func1(x) << std::endl;
+    std::cout << func2(y) << std::endl;
+    std::cout << j << std::endl;
+    std::cout << y << std::endl;
+}
