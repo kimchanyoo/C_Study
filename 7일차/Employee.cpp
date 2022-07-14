f20b5c554f6fc56a435129560146bc08700f76a2
Chanyoo Kim <cksdb012@gmail.com>
2022-07-14 22:37:52 +0900

    7일차 수정

--- 7일차/Employee.cpp
+++ 7일차/Employee.cpp
@@ -63,4 +63,35 @@ public:
 		employee_list[current_employee] = employee;
 		current_employee++;
 	}
+	int current_employee_num() { return current_employee; }
+
+	void print_employee_info() {
+		int total_pay = 0;
+		for (int i = 0; i < current_employee; i++) {
+			employee_list[i]->print_info();
+			total_pay += employee_list[i]->calculate_pay();
+		}
+		std::cout << "총 비용: " << total_pay << "만원" << std::endl;
+	}
+	~EmployeeList() {
+		for (int i = 0; i < current_employee; i++) {
+			delete employee_list[i];
+		}
+		delete[] employee_list;
+	}
 };
+
+int main() {
+	EmployeeList emp_list(10);
+	emp_list.add_employee(new Employee("노홍철", 34, "평사원", 1));
+	emp_list.add_employee(new Employee("하하", 34, "평사원", 1));
+
+	emp_list.add_employee(new Manager("유재석", 41, "부장", 7, 12));
+	emp_list.add_employee(new Manager("박명수", 43, "과장", 4, 15));
+	emp_list.add_employee(new Manager\("정준하", 43, "차장", 5, 13));
+	emp_list.add_employee(new Employee("정형돈", 36, "대리", 2));
+	emp_list.add_employee(new Employee("길", 36, "인턴", -2));
+	emp_list.print_employee_info();
+
+	return 0;
+}
